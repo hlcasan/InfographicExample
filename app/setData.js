828c197dc035a5e3a6074305c4c5622ec9534f18
Hugo L. Casanova <casanovh@vanier.college>
2019-09-28 23:09:23 -0400

    Added a check to avoid large values

--- app/setData.js
+++ app/setData.js
@@ -6,31 +6,37 @@ var insert_expense = function() {
     //When the user submits the form (clicks the button)
     addForm.addEventListener('submit', function (event) {
         event.preventDefault();
+        
+        if (document.getElementById("amount").value > 200) {
+        	alert("Please avoid large values that will break the interface. We all need to work with this app. And when extreme values are added, it breaks the chart's balance.");
+        } 
+        else {
 
-        //This is the backend file inserting in the DB
-        const php = "app/setData.php";
+					//This is the backend file inserting in the DB
+					const php = "app/setData.php";
 
-        //This is what we send to the server for the PHP file
-        const xhr = new XMLHttpRequest();
-        let formData = new FormData(addForm);
+					//This is what we send to the server for the PHP file
+					const xhr = new XMLHttpRequest();
+					let formData = new FormData(addForm);
 
-        //Connect to the PHP
-        xhr.open("POST", php, true);
-        xhr.onreadystatechange = function () {
-            console.log('readyState: ' + xhr.readyState);
-            console.log('status: ' + xhr.status);
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                // Everything ok, get the response
-                console.log(xhr.responseText);
+					//Connect to the PHP
+					xhr.open("POST", php, true);
+					xhr.onreadystatechange = function () {
+							console.log('readyState: ' + xhr.readyState);
+							console.log('status: ' + xhr.status);
+							if (xhr.readyState == 4 && xhr.status == 200) {
+									// Everything ok, get the response
+									console.log(xhr.responseText);
 
-                // Call a refresh of the list of names
-                chart();
-                //Reset the form
-                document.getElementById("addExpense").reset();
-                toggleForm();
-            }
-        };
-        xhr.send(formData);
+									// Call a refresh of the list of names
+									chart();
+									//Reset the form
+									document.getElementById("addExpense").reset();
+									toggleForm();
+							}
+					};
+					xhr.send(formData);
+				}
 
     });
 };
